654d819aea5905e14ceacc5d4fb5790568e2d8e2
Sean M. Snyder <sean.snyder@snhu.edu>
2016-02-29 20:34:31 -0500

    re-add database

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'action_controller/railtie'
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,5 +21,6 @@ module InteriorPainting
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
