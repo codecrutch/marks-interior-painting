b3c2f57a271f8e9a7aacec3182935e2ebeed052c
Sean M. Snyder <sean.snyder@snhu.edu>
2016-02-29 21:34:13 -0500

    add vendor/assets to path

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module InteriorPainting
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.assets.paths << "#{Rails.root}/vendor/assets"
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
