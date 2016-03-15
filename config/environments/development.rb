9a4f815869474230e5a96ee9b1e6a4d80d9cd5a6
Sean M. Snyder <sean.snyder@snhu.edu>
2016-03-14 22:13:09 -0400

    add sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,19 +14,21 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: “smtp.zoho.com”,
-    port: 465,
-    domain: ENV[“ZOHO_DOMAIN”],
-    authentication: “plain”,
-    enable_starttls_auto: true,
-    user_name: ENV[“ZOHO_USERNAME”],
-    password: ENV[“ZOHO_PASSWORD”]
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,15 +88,15 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default :charset => 'utf-8'
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.zoho.com",
-    port: 465,
-    domain: ENV["ZOHO_DOMAIN"],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["ZOHO_USERNAME"],
-    password: ENV["ZOHO_PASSWORD"]
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
 end
