ebd4c7facbe50df0a7174e94ca68cdc104cfcc84
Sean M. Snyder <sean.snyder@snhu.edu>
2016-05-04 10:28:11 -0400

    Fix sign-in position and make forms pretty

--- spec/mailers/estimate_mailer_spec.rb
+++ spec/mailers/estimate_mailer_spec.rb
@@ -2,16 +2,17 @@ require "rails_helper"
 
 RSpec.describe EstimateMailer, type: :mailer do
   describe "new_estimate_notification" do
-    let(:mail) { EstimateMailer.new_estimate_notification }
+    let(:estimate) { Estimate.new(customer_name: 'Bob', contact: '978-123-1234', inquiry: 'walls') }
+    let(:mail) { EstimateMailer.new_estimate_notification(estimate) }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("New estimate notification")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.subject).to eq("Job Estimate Request")
+      expect(mail.to).to eq(["markcentola@marksinteriorpainting.com"])
+      expect(mail.from).to eq(["admin@marksinteriorpainting.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to_not be_empty
     end
   end
 
